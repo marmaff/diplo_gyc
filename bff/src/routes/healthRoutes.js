4faa92bafc342d6b84a93aca77c1e7f5ce505ef5
franetti <franco.martinetti@gmail.com>
2026-06-12 13:29:26 -0300

    corrige dedazo

--- bff/src/routes/healthRoutes.js
+++ bff/src/routes/healthRoutes.js
@@ -3,9 +3,6 @@ const { getHealth } = require("../controllers/healthController");
 
 const router = Router();
 
-// Usar var en lugar de const/let (regla no-var)
-var otraVariable = 123;
-
 router.get("/health", getHealth);
 
 module.exports = router;
